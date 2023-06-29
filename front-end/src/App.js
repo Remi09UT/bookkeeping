240ab07372926afed36a004d85da2379c6353e1a
Tony Park <tonypiao617@gmail.com>
2023-06-29 10:34:31 -0400

    navigate to login from home page without JWT

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "./comps/Home";
 import LoginPage from "./comps/LoginPage";
 import RegisterPage from "./comps/RegisterPage";
@@ -10,7 +15,16 @@ function App() {
       <Routes>
         <Route exact path="/Login" element={<LoginPage />} />
         <Route path="/Register" element={<RegisterPage />} />
-        <Route path="/Home" element={<Home />} />
+        <Route
+          path="/Home"
+          element={
+            sessionStorage.getItem("bookKeepingCredential") ? (
+              <Home />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
       </Routes>
     </Router>
   );
