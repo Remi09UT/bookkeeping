7aa5e28ca5eba334719faadab5abd7f747c8af47
Tony Park <tonypiao617@gmail.com>
2023-07-04 01:07:15 -0400

    return response in each then clause in ProgressBar

--- front-end/src/comps/ProgressBar.js
+++ front-end/src/comps/ProgressBar.js
@@ -17,77 +17,79 @@ export default function ProgressBar({
   useEffect(() => {
     const JWT = sessionStorage.getItem("bookKeepingCredential");
     setStatus("loading picture~");
-    let callback = async function () {
-      let response;
-      try {
-        response = await axios.get(URL + "uploads/static/" + file.name, {
-          headers: {
-            Authorization: `Bearer ${JWT}`,
-          },
-        });
-
-        // upload picture to GCP storage URL
-        // bucketFileName.current = response.data.bucketFileName;
-        //     await axios.put(response.data.url, file, {
-        //       headers: {
-        //         "Content-Type": "application/octet-stream",
-        //       },
-        //     });
-        //     console.log("step2: upload picture to GCP");
-        //     console.log(response);
-      } catch (error) {
-        console.log(error.message);
-      }
-
-      console.log(response);
-    };
-
-    callback();
-
-    // axios
-    //   .get(URL + "uploads/static/" + file.name, {
-    //     headers: {
-    //       Authorization: `Bearer ${JWT}`,
-    //     },
-    //   })
-    //   .then(async (response) => {
-    //     // upload picture to GCP storage URL
-    //     bucketFileName.current = response.data.bucketFileName;
-    //     await axios.put(response.data.url, file, {
+    // let callback = async function () {
+    //   let response;
+    //   try {
+    //     response = await axios.get(URL + "uploads/static/" + file.name, {
     //       headers: {
-    //         "Content-Type": "application/octet-stream",
+    //         Authorization: `Bearer ${JWT}`,
     //       },
     //     });
-    //     console.log("step2: upload picture to GCP");
-    //     console.log(response);
-    //   })
-    //   .then(async (response) => {
-    //     // request document AI analyze receipt
-    //     setStatus("AI analyzing receipt~");
-    //     console.log(bucketFileName.current);
-    //     await axios.post(
-    //       URL + "receipts/",
-    //       { bucketFileName: bucketFileName.current, fileType: "png" },
-    //       {
-    //         headers: {
-    //           Authorization: `Bearer ${JWT}`,
-    //           ContentType: "application/json",
-    //           // Add any additional headers if required
-    //         },
-    //       }
-    //     );
-    //     console.log("step3: request document AI");
-    //     console.log(response);
-    //   })
-    //   .then(async (response) => {
-    //     //TODO: handle document AI result
-    //     console.log("step4: show AI result");
-    //     console.log(response);
-    //   })
-    //   .catch((error) => {
-    //     // Handle error
-    //     alert(error.message);
-    //   });
+
+    //     // upload picture to GCP storage URL
+    //     // bucketFileName.current = response.data.bucketFileName;
+    //     //     await axios.put(response.data.url, file, {
+    //     //       headers: {
+    //     //         "Content-Type": "application/octet-stream",
+    //     //       },
+    //     //     });
+    //     //     console.log("step2: upload picture to GCP");
+    //     //     console.log(response);
+    //   } catch (error) {
+    //     console.log(error.message);
+    //   }
+
+    //   console.log(response);
+    // };
+
+    // callback();
+
+    axios
+      .get(URL + "uploads/static/" + file.name, {
+        headers: {
+          Authorization: `Bearer ${JWT}`,
+        },
+      })
+      .then(async (response) => {
+        // upload picture to GCP storage URL
+        bucketFileName.current = response.data.bucketFileName;
+        const curResponse = await axios.put(response.data.url, file, {
+          headers: {
+            "Content-Type": "application/octet-stream",
+          },
+        });
+        console.log("step2: upload picture to GCP");
+        console.log(response);
+        return curResponse;
+      })
+      .then(async (response) => {
+        // request document AI analyze receipt
+        setStatus("AI analyzing receipt~");
+        console.log(bucketFileName.current);
+        const curResponse = await axios.post(
+          URL + "receipts/",
+          { bucketFileName: bucketFileName.current, fileType: "png" },
+          {
+            headers: {
+              Authorization: `Bearer ${JWT}`,
+              ContentType: "application/json",
+              // Add any additional headers if required
+            },
+          }
+        );
+        console.log("step3: request document AI");
+        console.log(response);
+        return curResponse;
+      })
+      .then((response) => {
+        //TODO: handle document AI result
+        console.log("step4: show AI result");
+        console.log(response);
+      })
+      .catch((error) => {
+        // Handle error
+        alert(error.message);
+      });
   }, []);
 
   return (
