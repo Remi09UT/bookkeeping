8eb3c4e01de3b4f306aa6d2fe24020d882609ab6
RuiWang98 <benjaminwr98@gmail.com>
2023-07-19 14:32:47 -0400

    Fixed closed the connection

--- back-end/tidb/config.js
+++ back-end/tidb/config.js
@@ -1,78 +1,52 @@
-var mysql = require('mysql2');
+const mysql = require("mysql2");
 
-var connection = mysql.createConnection({
-  host: 'gateway01.eu-central-1.prod.aws.tidbcloud.com',
+var db_config = {
+  host: "gateway01.eu-central-1.prod.aws.tidbcloud.com",
   port: 4000,
-  user: '3tct5ZhdBXiMUDP.root',
-  password: 'FaZ24NgO4jZfQnWu',
-  database: 'recipt',
+  user: "3tct5ZhdBXiMUDP.root",
+  password: "FaZ24NgO4jZfQnWu",
+  database: "recipt",
   ssl: {
-    minVersion: 'TLSv1.2',
-    rejectUnauthorized: true
-  }
-});
+    minVersion: "TLSv1.2",
+    rejectUnauthorized: true,
+  },
+};
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config);
+
+  connection.connect(function (err) {
+    if (err) {
+      console.error("TiDB connection error:", err);
+      setTimeout(handleDisconnect, 2000);
+    } else {
+      console.log("connected to tidb");
+    }
+  });
+
+  connection.on("error", function (err) {
+    console.error("TiDB db error", err);
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
 
 function createConnection() {
-    return new Promise((resolve, reject) => {
-      connection.connect((err) => {
-        if (err) {
-          console.error('TiDB connection error:', err);
-          reject(err);
-          return;
-        }
-        resolve(connection);
-        console.log("connected to tidb");
-      });
-    });
-  }
-
-// const mysql = require('mysql2/promise');
-
-// const pool = mysql.createPool({
-//   host: 'gateway01.eu-central-1.prod.aws.tidbcloud.com',
-//   port: 4000,
-//   user: '3tct5ZhdBXiMUDP.root',
-//   password: 'FaZ24NgO4jZfQnWu',
-//   database: 'recipt',
-//   ssl: {
-//     minVersion: 'TLSv1.2',
-//     rejectUnauthorized: true
-//   }
-// });
-
-// async function createConnection() {
-//   try {
-//     const connection = await pool.getConnection();
-//     console.log('Connected to TiDB');
-//     return connection;
-//   } catch (error) {
-//     console.error('TiDB connection error:', error);
-//     throw error;
-//   }
-// }
+  return new Promise((resolve, reject) => {
+    if (connection && connection.state === "connected") {
+      resolve(connection);
+    } else {
+      handleDisconnect();
+      resolve(connection);
+    }
+  });
+}
 
 module.exports = {
   createConnection,
 };
-
-
-// function createConnection() {
-//     const connection = mysql.createConnection({
-//       host: 'gateway01.eu-central-1.prod.aws.tidbcloud.com',
-//       port: 4000,
-//       user: '3tct5ZhdBXiMUDP.root',
-//       password: 'FaZ24NgO4jZfQnWu',
-//       database: 'recipt',
-//       ssl: {
-//         minVersion: 'TLSv1.2',
-//         rejectUnauthorized: true
-//       }
-//     });
-  
-//     return connection.promise();
-//   }
-  
-  
-  module.exports = {
-    createConnection,
-  };
